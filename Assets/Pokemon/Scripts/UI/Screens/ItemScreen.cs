bd97edf40a31bad7164d19e6fd765d4f5837db58
Nguyen Quan <gon28032004@gmail.com>
2026-04-05 00:20:05 +0700

    feat: item screen, mission screen

---
+++ Assets/Pokemon/Scripts/UI/Screens/ItemScreen.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+namespace Pokemon.Scripts.UI.Screens
+{
+    public class ItemScreen : MonoBehaviour
+    {
+        private void Start()
+        {
+        }
+
+        private void Update()
+        {
+        }
+    }
+}
---
+++ Assets/Pokemon/Scripts/UI/Screens/MissionScreen.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+namespace Pokemon.Scripts.UI.Screens
+{
+    public class MissionScreen : MonoBehaviour
+    {
+        private void Start()
+        {
+        }
+
+        private void Update()
+        {
+        }
+    }
+}
