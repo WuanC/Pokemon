80e795961bdf5c22808de496db6e597da87c303c
Nguyen Quan <gon28032004@gmail.com>
2026-04-05 22:43:27 +0700

    feat: compatible type

--- Assets/Pokemon/Scripts/Pokemon/Type.cs
+++ Assets/Pokemon/Scripts/Pokemon/Type.cs
@@ -21,17 +21,41 @@ namespace Pokemon.Scripts.Pokemon
         {
            //        F, W, E, E, D, S, D, Gold 
         new float[] {1f, 0.5f, 1f, 2f, 1f, 2f, 0.5f, 1f}, // Fire
-        new float[] {2f, 0.5f, 1f, 2f, 1f, 1f, 0.5f, 1f}, // Water
-        new float[] {1f, 2f, 0.5f, 0f, 1f, 1f, 1f, 1f},   // Electric
-        new float[] {0.5f, 1f, 2f, 0.5f, 2f, 1f, 2f, 1f}, // Earth
-        new float[] {1f, 1f, 1f, 1f, 0.5f, 2f, 0.5f, 1f}, // Dark
-        new float[] {1f, 1f, 1f, 1f, 0.5f, 0.5f, 2f, 1f}, // Spirit
-        new float[] {2f, 1f, 1f, 0.5f, 2f, 0.5f, 0.5f, 1f}, // Diamond
+        new float[] {2f, 1f, 1f, 2f, 1f, 1f, 0.5f, 1f}, // Water
+        new float[] {1f, 2f, 1f, 0f, 1f, 1f, 1f, 1f},   // Electric
+        new float[] {0.5f, 1f, 2f, 1f, 2f, 1f, 2f, 1f}, // Earth
+        new float[] {1f, 1f, 1f, 1f, 1f, 2f, 0.5f, 1f}, // Dark
+        new float[] {1f, 1f, 1f, 1f, 0.5f, 1, 2f, 1f}, // Spirit
+        new float[] {2f, 1f, 1f, 0.5f, 2f, 0.5f, 1f, 1f}, // Diamond
         new float[] {1f, 1f, 1f, 1f, 1f, 1f, 1f, 1f}, // Gold
 };
         public static float GetEffectiveness(PkmType attackType, PkmType defenseType)
         {
             return chart[(int)attackType][(int)defenseType];
+        }
+        public static List<PkmType> GetStrongType(PkmType pkmType)
+        {
+            List<PkmType> strongTypes = new List<PkmType>();
+            for (int i = 0; i < chart.Length; i++)
+            {
+                if (chart[(int)pkmType][i] < 1f)
+                {
+                    strongTypes.Add((PkmType)i);
+                }
+            }
+            return strongTypes;
+        }
+        public static List<PkmType> GetWeakType(PkmType pkmType)
+        {
+            List<PkmType> weakTypes = new List<PkmType>();
+            for (int i = 0; i < chart.Length; i++)
+            {
+                if (chart[(int)pkmType][i] > 1f)
+                {
+                    weakTypes.Add((PkmType)i);
+                }
+            }
+            return weakTypes;
         }
     }
 }
--- Assets/Pokemon/Scripts/UI/CompatibleType.cs
+++ Assets/Pokemon/Scripts/UI/CompatibleType.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using Pokemon.Scripts.Data;
 using Pokemon.Scripts.Pokemon;
@@ -17,6 +18,32 @@ namespace Pokemon.Scripts.UI
         public void SetupCompatibleType(PkmType type)
         {
             currentIcon.sprite = typeData.symbols.FirstOrDefault(x => x.type == type).icon;
+            List<PkmType> strongTypes = TypeChart.GetStrongType(type);
+            List<PkmType> weakTypes = TypeChart.GetWeakType(type);
+            for (int i = 0; i < strongIcon.Length; i++)
+            {
+                if (i < strongTypes.Count)
+                {
+                    strongIcon[i].sprite = typeData.symbols.FirstOrDefault(x => x.type == strongTypes[i]).icon;
+                    strongIcon[i].gameObject.SetActive(true);
+                }
+                else
+                {
+                    strongIcon[i].gameObject.SetActive(false);
+                }
+            }
+            for (int i = 0; i < weakIcon.Length; i++)
+            {
+                if (i < weakTypes.Count)
+                {
+                    weakIcon[i].sprite = typeData.symbols.FirstOrDefault(x => x.type == weakTypes[i]).icon;
+                    weakIcon[i].gameObject.SetActive(true);
+                }
+                else
+                {
+                    weakIcon[i].gameObject.SetActive(false);
+                }
+            }
         }
 
     }
