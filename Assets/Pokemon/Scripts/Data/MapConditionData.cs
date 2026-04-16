ef4abb50f4ff599ad81e2316270202416257c626
Nguyen Quan <gon28032004@gmail.com>
2026-04-16 12:21:15 +0700

    feat: update hub ui

---
+++ Assets/Pokemon/Scripts/Data/MapConditionData.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+namespace Pokemon.Scripts.Data
+{
+    public enum MapConditionType
+    {
+        None,
+        UnlockMap,
+        Pay,
+    }
+    [CreateAssetMenu(fileName = "MapConditionData", menuName = "Pokemon/MapConditionData")]
+    public class MapConditionData : ScriptableObject
+    {
+        public MapConditionType conditionType;
+        //For UnlockMap
+        public MapData requiredMap;
+        public float progressRequired;
+
+        //For Pay
+        public int goldRequired;
+    }
+}
