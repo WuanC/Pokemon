700dd0cbf7e9d26d1febcc67cfc8ef1e4f6c3b97
Nguyen Quan <gon28032004@gmail.com>
2026-05-02 00:46:40 +0700

    feat: update skill

--- Assets/Pokemon/Scripts/Battle/SkillFx.cs
+++ Assets/Pokemon/Scripts/Battle/SkillFx.cs
@@ -1,36 +1,76 @@
 using System.Collections;
+using DG.Tweening;
 using Pokemon.Scripts.MyUtils.ObjectPooling;
-using Unity.VisualScripting;
 using UnityEngine;
 
 namespace Pokemon.Scripts.Battle
 {
+    public enum SkillFxType
+    {
+        Default,
+        ManyProjectiles,
+    }
     [CreateAssetMenu(fileName = "New Skill Fx", menuName = "Pokemon/Skills/Skill Fx")]
     public class SkillFx : ScriptableObject
     {
         [SerializeField] protected GameObject targetFxPrefab;
         [SerializeField] protected GameObject ownerFxPrefab;
         [SerializeField] protected float fxDuration;
+        [SerializeField] protected SkillFxType fxType;
         public IEnumerator CastEffectCoroutine(BattlePokemon owner, BattlePokemon target)
         {
-            GameObject ownerFx = null;
-            GameObject targetFx = null;
-            if (ownerFxPrefab != null)
+
+            if (fxType == SkillFxType.Default)
             {
-                ownerFx = MyPoolManager.Instance.GetFromPool(ownerFxPrefab);
-                ownerFx.transform.position = owner.pokemonImage.transform.position;
+                GameObject ownerFx = null;
+                GameObject targetFx = null;
+                if (ownerFxPrefab != null)
+                {
+                    ownerFx = MyPoolManager.Instance.GetFromPool(ownerFxPrefab);
+                    ownerFx.transform.position = owner.pokemonImage.transform.position;
+
+                }
+                if (targetFxPrefab != null)
+                {
+                    targetFx = MyPoolManager.Instance.GetFromPool(targetFxPrefab);
+                    targetFx.transform.position = target.pokemonImage.transform.position;
+                    target.HitAnimation();
+                }
+                yield return new WaitForSeconds(fxDuration);
+                if (ownerFx != null) ownerFx.SetActive(false);
+                if (targetFx != null) targetFx.SetActive(false);
 
             }
-            if (targetFxPrefab != null)
+            else if (fxType == SkillFxType.ManyProjectiles)
             {
-                targetFx = MyPoolManager.Instance.GetFromPool(targetFxPrefab);
-                targetFx.transform.position = target.pokemonImage.transform.position;
-                target.HitAnimation();
-            }
-            yield return new WaitForSeconds(fxDuration);
-            if (ownerFx != null) ownerFx.SetActive(false);
-            if (targetFx != null) targetFx.SetActive(false);
+                if (ownerFxPrefab != null)
+                {
+                    Sequence sequence = DOTween.Sequence();
 
+                    for (int i = 0; i < 5; i++)
+                    {
+                        float spawnDelay = i * 0.1f;
+
+                        sequence.Insert(spawnDelay, DOTween.Sequence().AppendCallback(() =>
+                        {
+                            GameObject projectile = MyPoolManager.Instance.GetFromPool(ownerFxPrefab);
+
+                            projectile.transform.position = owner.pokemonImage.transform.position;
+
+                            projectile.transform.DOMove(
+                                target.pokemonImage.transform.position,
+                                fxDuration
+                            ).OnComplete(() =>
+                            {
+                                projectile.SetActive(false);
+                                target.HitAnimation();
+                            });
+                        }));
+                    }
+
+                    yield return sequence.WaitForCompletion();
+                }
+            }
         }
     }
 }
